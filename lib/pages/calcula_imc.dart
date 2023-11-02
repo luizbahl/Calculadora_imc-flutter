6e1b83d4312595d0d6a4bf974aca949b97b83876
LuizBahl <luizbahl@hotmail.com>
2023-11-02 00:17:35 -0300

    Criando função IMC e ajustes da calculaIMC_page

--- lib/pages/calcula_imc.dart
+++ lib/pages/calcula_imc.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CalculaImc extends StatefulWidget {
   const CalculaImc({Key? key}) : super(key: key);
@@ -8,6 +9,37 @@ class CalculaImc extends StatefulWidget {
 }
 
 class _CalculaImcState extends State<CalculaImc> {
+  var pesoController = TextEditingController(text: "");
+  var alturaController = TextEditingController(text: "");
+
+  double peso = 0;
+  double altura = 0;
+  double imc = 0;
+
+  String calcularNivelIMC(double imc) {
+    if (imc < 18.5) {
+      return "Abaixo do Peso";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      return "Peso Normal";
+    } else if (imc >= 25.0 && imc <= 29.9) {
+      return "Sobrepeso";
+    } else if (imc >= 30.0 && imc <= 34.9) {
+      return "Obesidade Grau I";
+    } else if (imc >= 35.0 && imc <= 39.9) {
+      return "Obesidade Grau II";
+    } else {
+      return "Obesidade Grau III";
+    }
+  }
+
+  void calculadoraIMC() {
+    if (altura > 0 && peso > 0) {
+      setState(() {
+        imc = peso / (altura * altura);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +54,17 @@ class _CalculaImcState extends State<CalculaImc> {
             child: Column(
               children: [
                 const SizedBox(
-                  height: 80,
+                  height: 100,
                 ),
                 TextField(
-                  onChanged: (ValueKey) {},
+                  style: GoogleFonts.amita(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  controller: pesoController, // arrumar adicionar variável
+                  onChanged: (value) {},
                   decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.only(bottom: 9),
+                      contentPadding: EdgeInsets.only(bottom: 2, top: 13),
                       enabledBorder: UnderlineInputBorder(
                           borderSide: BorderSide(
                               color: Color.fromARGB(255, 209, 209, 209))),
@@ -44,19 +81,24 @@ class _CalculaImcState extends State<CalculaImc> {
                       )),
                 ),
                 const SizedBox(
-                  height: 100,
+                  height: 80,
                 ),
                 TextField(
-                  onChanged: (ValueKey) {},
+                  style: GoogleFonts.amita(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  controller: alturaController, // arrumar adicionar variável
+                  onChanged: (value) {},
                   decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.only(bottom: 9),
+                      contentPadding: EdgeInsets.only(bottom: 2, top: 13),
                       enabledBorder: UnderlineInputBorder(
                           borderSide: BorderSide(
                               color: Color.fromARGB(255, 209, 209, 209))),
                       focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(
                               color: Color.fromARGB(255, 209, 209, 209))),
-                      hintText: "Altura",
+                      hintText: "Altura em metros",
                       hintStyle: TextStyle(
                           color: Colors.black, fontWeight: FontWeight.w700),
                       prefixIcon: Icon(
@@ -65,7 +107,32 @@ class _CalculaImcState extends State<CalculaImc> {
                         size: 40,
                       )),
                 ),
-                const SizedBox(height: 50),
+                const SizedBox(height: 80),
+                TextButton(
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Color.fromARGB(255, 209, 209, 209))),
+                  onPressed: calculadoraIMC,
+                  child: const Text(
+                    'Calcular IMC',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(height: 60),
+                Text(
+                  'Seu IMC é: ${imc.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  calcularNivelIMC(imc),
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w700),
+                )
               ],
             ),
           ),
